cdf206a9df193b11bce856c96e02d957e3650c06
Simon Walker <simon.walker@the-mtc.org>
2018-06-01 15:14:16 +0100

    Initial commit

---
+++ gentest.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+
+# Set up keras to use tensorflow
+import os
+os.environ['KERAS_BACKEND'] = 'tensorflow'
+
+import keras
+assert keras.__version__ == '2.1.5'
+import numpy as np
+from keras.preprocessing.image import ImageDataGenerator, NumpyArrayIterator
+import keras
+
+
+def is_onehot(y):
+    return len(y.shape) > 1
+
+class CustomArrayIterator(NumpyArrayIterator):
+    def __init__(self, x, y, image_data_generator,
+            batch_size=32, shuffle=False, seed=None,
+            data_format=None, save_to_dir=None,
+            save_prefix='', save_format='png',
+            subset=None):
+        super(CustomArrayIterator, self).__init__(x, y, image_data_generator,
+                batch_size=batch_size, shuffle=shuffle, seed=seed,
+                data_format=data_format, save_to_dir=save_to_dir,
+                save_prefix=save_prefix, save_format=save_format,
+                subset=subset)
+
+        if is_onehot(y):
+            raise NotImplementedError('One-hot encoding not supported for now')
+        else:
+            self.good_idx = np.where(y == 0)[0]
+            self.defect_idx = np.where(y == 1)[0]
+
+    def _get_batches_of_transformed_samples(self, index_array):
+        print('CUSTOM BATCH')
+        print(index_array)
+        return super(CustomArrayIterator, self)._get_batches_of_transformed_samples(index_array)
+
+
+    def _flow_index(self):
+        print('CALLING FLOW INDEX!!!')
+        self.reset()
+        self._set_index_array()
+        while True:
+            if self.seed is not None:
+                np.random.seed(self.seed + self.total_batches_seen)
+
+            class_size = self.batch_size // 2
+
+            defect_idx = np.random.choice(self.defect_idx, replace=False, size=class_size)
+            good_idx = np.random.choice(self.good_idx, replace=False, size=class_size)
+            idx = np.hstack([good_idx, defect_idx])
+            idx = np.random.permutation(idx)
+
+            self.total_batches_seen += 1
+            yield self._get_batches_of_transformed_samples(idx)
+
+class CustomGenerator(ImageDataGenerator):
+    def flow(self, x, y=None, batch_size=32, shuffle=True, seed=None,
+            save_to_dir=None, save_prefix='', save_format='png', subset=None):
+        return CustomArrayIterator(x, y, self,
+                batch_size=batch_size,
+                shuffle=shuffle,
+                seed=seed,
+                data_format=self.data_format,
+                save_to_dir=save_to_dir,
+                save_prefix=save_prefix,
+                save_format=save_format,
+                subset=subset)
+
+
+
+if __name__ == '__main__':
+
+
+    n_images = 200
+    shape = (200, 32, 32, 1)
+    X = np.random.uniform(0., 1., size=shape)
+    y = np.zeros(n_images, dtype=int)
+
+    y[:20] = 1
+    
+    batch_size = 32
+    generator = CustomGenerator()
+    gen = generator.flow(X, y, batch_size=batch_size)
+
+    n_iter = 10
+    for i in range(10):
+        _, y_batch = next(gen)
+        idx = y_batch == 0
+        n_0 = idx.size
+        n_target = batch_size // 2
+
+        msg = f'Iteration {i + 1}/{n_iter}: class size for 0 is {n_0}, should be {n_target}'
+
+        assert  n_0 == batch_size // 2, msg
+
