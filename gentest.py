65f5b786535845133f29160b65a537b16dbc7037
Simon Walker <s.r.walker101@googlemail.com>
2018-06-02 20:35:45 +0100

    Support one-hot encoding

--- gentest.py
+++ gentest.py
@@ -9,28 +9,36 @@ assert keras.__version__ == '2.1.5'
 import numpy as np
 from keras.preprocessing.image import ImageDataGenerator, NumpyArrayIterator
 import itertools
-import time
 import keras
 
 
 def is_onehot(y):
-    return len(y.shape) > 1
+    return len(y.shape) == 2
 
-class CustomArrayIterator(NumpyArrayIterator):
+class OversamplingArrayIterator(NumpyArrayIterator):
     def __init__(self, x, y, image_data_generator,
             batch_size=32, shuffle=False, seed=None,
             data_format=None, save_to_dir=None,
             save_prefix='', save_format='png',
             subset=None):
-        super(CustomArrayIterator, self).__init__(x, y, image_data_generator,
+        super(OversamplingArrayIterator, self).__init__(x, y, image_data_generator,
                 batch_size=batch_size, shuffle=shuffle, seed=seed,
                 data_format=data_format, save_to_dir=save_to_dir,
                 save_prefix=save_prefix, save_format=save_format,
                 subset=subset)
 
         if is_onehot(y):
-            raise NotImplementedError('One-hot encoding not supported for now')
+            n_classes = y.shape[1]
+            if n_classes != 2:
+                raise ValueError('Only binary classification is supported')
+
+            self.good_idx = np.where(y[:, 0] == 1)[0]
+            self.defect_idx = np.where(y[:, 1] == 1)[0]
         else:
+            n_classes = np.unique(y)
+            if n_classes != 2:
+                raise ValueError('Only binary classification is supported')
+
             self.good_idx = np.where(y == 0)[0]
             self.defect_idx = np.where(y == 1)[0]
 
@@ -52,10 +60,10 @@ class CustomArrayIterator(NumpyArrayIterator):
             yield idx
 
 
-class CustomGenerator(ImageDataGenerator):
+class OversamplingGenerator(ImageDataGenerator):
     def flow(self, x, y=None, batch_size=32, shuffle=True, seed=None,
             save_to_dir=None, save_prefix='', save_format='png', subset=None):
-        return CustomArrayIterator(x, y, self,
+        return OversamplingArrayIterator(x, y, self,
                 batch_size=batch_size,
                 shuffle=shuffle,
                 seed=seed,
@@ -72,27 +80,22 @@ if __name__ == '__main__':
     n_images = 200
     shape = (200, 32, 32, 1)
     X = np.random.uniform(0., 1., size=shape)
-    y = np.zeros(n_images, dtype=int)
-    y[:20] = 1
+    y = np.zeros((n_images, 2), dtype=int)
+    y[:20, 1] = 1
+    y[20:, 0] = 1
     
     batch_size = 32
-    generator = CustomGenerator()
+    generator = OversamplingGenerator()
     gen = generator.flow(X, y, batch_size=batch_size)
 
     n_iter = 10
-    start_time = time.time()
 
-    while True:
+    for _ in range(n_iter):
         _, y_batch = next(gen)
-        idx = np.where(y_batch == 0)[0]
+        idx = np.where(y_batch[:, 0] == 0)[0]
         n_0 = idx.size
         n_target = batch_size // 2
 
         msg = f'Class size for 0 is {n_0}, should be {n_target}'
 
         assert  n_0 == batch_size // 2, msg
-
-        if (time.time() - start_time) > 10:
-            break
-
-        
