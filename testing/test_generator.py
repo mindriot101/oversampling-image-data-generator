fa4386f9e5e4810ec79822c86afe8268afeb2579
Simon Walker <s.r.walker101@googlemail.com>
2018-06-03 19:47:38 +0100

    Test preprocessing function

--- testing/test_generator.py
+++ testing/test_generator.py
@@ -23,6 +23,7 @@ def test_onehot():
 
     assert  n_0 == batch_size // 2, msg
 
+
 def test_not_onehot():
     n_images = 200
     shape = (200, 32, 32, 1)
@@ -42,3 +43,20 @@ def test_not_onehot():
     msg = f'Class size for 0 is {n_0}, should be {n_target}'
 
     assert  n_0 == batch_size // 2, msg
+
+
+def test_datagen_methods_still_work():
+    n_images = 200
+    shape = (200, 32, 32, 1)
+    X = np.random.uniform(0., 1., size=shape)
+    y = np.zeros(n_images, dtype=int)
+    y[:20] = 1
+
+    batch_size = 32
+    generator = OversamplingGenerator(
+            preprocessing_function=lambda X: X + 50,
+            )
+    gen = generator.flow(X, y, batch_size=batch_size)
+
+    X_batch, _ = next(gen)
+    assert X_batch.min() > 2
